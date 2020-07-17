5a7528fbd66defcf65e1b49b17932704a2aae65d
Yusuke Tanaka <yusuktan@maguro.dev>
2020-07-17 22:58:22 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-await-in-loop": "error",
+    "no-control-regex": "error",
+    eqeqeq: "error",
+  },
+};
---
+++ index.ts
@@ -0,0 +1,5 @@
+declare namespace Foo {
+  export function foo(s: string): void;
+  export function bar(): void;
+  export function foo(sn: string | number): void;
+}
