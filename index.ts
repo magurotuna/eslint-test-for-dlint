dd8eaa52f10da857320a447f2ff35c06ab31e3f9
Yusuke Tanaka <yusuktan@maguro.dev>
2020-07-18 01:59:58 +0900

    adjacent-overload-signatures test

--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
-declare namespace Foo {
-  export function foo(s: string): void;
-  export function bar(): void;
-  export function foo(sn: string | number): void;
+interface Foo {
+  (s: string): void;
+  foo(n: number): void;
+  (n: number): void;
+  (sn: string | number): void;
+  bar(): void;
+  baz(): void;
+  call(): number;
+}
+
+interface Bar {
+  (s: string): void;
+  foo(): void;
+  call(): number;
+}
+
+class Baz {
+  constructor() {
+    console.log();
+  }
+  foo() {
+    console.log();
+  }
+  new() {
+    console.log();
+  }
 }
