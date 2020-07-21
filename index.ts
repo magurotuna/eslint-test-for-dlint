8a00449a53e16b4869c31b7b42ded192470a6fdb
Yusuke Tanaka <yusuktan@maguro.dev>
2020-07-22 00:23:59 +0900

    test for `constructor-super`

--- index.ts
+++ index.ts
@@ -1,27 +1,5 @@
-interface Foo {
-  (s: string): void;
-  foo(n: number): void;
-  (n: number): void;
-  (sn: string | number): void;
-  bar(): void;
-  baz(): void;
-  call(): number;
-}
-
-interface Bar {
-  (s: string): void;
-  foo(): void;
-  call(): number;
-}
-
-class Baz {
+class A extends null {
   constructor() {
-    console.log();
-  }
-  foo() {
-    console.log();
-  }
-  new() {
-    console.log();
+    console.log("a");
   }
 }
