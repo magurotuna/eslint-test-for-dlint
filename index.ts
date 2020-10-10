939ac82b3af9607c7ff1ce1c0ad1291cc52e8a1c
Yusuke Tanaka <yusuktan@maguro.dev>
2020-10-10 23:15:45 +0900

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 function foo() {
-  do {
-    break;
-  } while (a);
-  return 1;
+  for (const i of [1, 2]) {
+    return i;
+  }
+  console.log(1);
+}
+
+function bar() {
+  for (const i in {}) {
+    return i;
+  }
+  console.log(1);
+}
+
+function bar2() {
+  try {
+    return 1;
+  } finally {
+    console.log(1);
+  }
 }
